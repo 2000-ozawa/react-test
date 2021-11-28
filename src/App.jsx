ea5fb3cbecbdb8f7b34eba552ba031c54a21faf1
Ozawa Ryo <92570622+2000-ozawa@users.noreply.github.com>
2021-11-28 15:21:14 +0900

    分割代入 propsの使い方の少し応用的なもの

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import ColorfullMessage from "./components/ColorfullMessage";
 
 const App = () => {
   const onClickButton = () => alert();
-  const contentLadyStyle = {
-    color: "pink",
-    fontSize: "18px"
-  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <ColorfullMessage color="blue" message="お元気ですか？" />
-      <p style={contentLadyStyle}>元気です！</p>
+      <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
+      <ColorfullMessage color="pink">元気です！</ColorfullMessage>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
