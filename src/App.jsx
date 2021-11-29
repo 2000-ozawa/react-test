7b1deaeb0b6e6d537fd19899f935b86b09aa0111
Ozawa Ryo <92570622+2000-ozawa@users.noreply.github.com>
2021-11-30 08:06:46 +0900

    表示/非表示の変更処理を実装 ボタンを押すことで要素の表示/非表示が入れ替わる処理を実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,26 @@ import React, { useState } from "react";
 import ColorfullMessage from "./components/ColorfullMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
       <ColorfullMessage color="pink">元気です！</ColorfullMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(^^)</p>}
     </>
   );
 };
